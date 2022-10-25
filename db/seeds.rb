5b5923e3d9e9735ffeec95dd61b802c9f7bb7aef
Freddy Henderson <hendersonsarah12@gmail.com>
2022-10-25 19:21:25 +0200

    added a seed.db file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Creature.destroy_all
+
+10.times do
+  Creature.create(name: Faker::TvShows::Supernatural.creature,
+                  age: Faker::Number.within(range: 1..10_000),
+                  ability: Faker::Superhero.power,
+                  description: Faker::Restaurant.review)
+end
