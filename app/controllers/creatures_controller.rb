fe78974796a1f784d0924f3d2c620e6ec1340bde
Freddy Henderson <hendersonsarah12@gmail.com>
2022-10-27 21:35:56 +0200

    fixed issue

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -12,28 +12,29 @@ class CreaturesController < ApplicationController
     @creature = Creature.new(creature_params)
     if @creature.save
       redirect_to creatures_path
-    else 
-      puts "Fatal error my friend"    
+    else
+      puts "Fatal error my friend"
     end
   end
-  
+
   def show
     @creature = Creature.find(params[:id])
   end
 
   def destroy
     Creature.destroy
-    redirect_to creatures_path 
+    redirect_to creatures_path
   end
-  
+
   def adopt
     @user = current_user
     @creature = Creature.find(params[:creature_id])
     @creature.user = @user
   end
-  
+
   private
 
-def creature_params
+  def creature_params
     params.require(:creature).permit(:name, :age, :ability, :description)
+  end
 end
