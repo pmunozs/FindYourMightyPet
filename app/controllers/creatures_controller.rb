4a97ca3b0435d983cc08b8f8173c70672623a93c
augustin romaneschi <augaugro@gmail.com>
2022-10-27 21:12:56 +0200

    Add new creature

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -3,4 +3,29 @@ class CreaturesController < ApplicationController
   def index
     @creatures = Creature.all
   end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    @creature = Creature.new(creature_params)
+    if @creature.save
+      redirect_to creatures_path
+    else 
+      puts "Fatal error my friend"    
+    end
+  end
+
+  def destroy
+    Creature.destroy
+    redirect_to creatures_path 
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :age, :ability, :description)
+  end
+
 end
